24478ec0fc55fc702a11ad46522a94323a20e123
Igor Galvao <igorgbueno@gmail.com>
2020-11-18 15:28:30 -0300

    Setup basic React components

---
+++ app/javascript/components/Character/Character.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Character = () => {
+  return <div>Characters#show</div>
+}
+
+export default Character
---
+++ app/javascript/components/Characters/Characters.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Characters = () => {
+  return <div>Characters#index</div>
+}
+
+export default Characters
